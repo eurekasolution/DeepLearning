e3cc3cd27f384696879651a47107bb4a3e1fb7c6
eurekasolution <nadopro@gmail.com>
2021-11-11 16:04:15 +0900

    03topic-1

--- 02wordcloud.py
+++ 02wordcloud.py
@@ -9,6 +9,16 @@ documents=[
     '나는 아침에 라면을 자주 먹는다.',
     '나느 아침에 밥 대신에 라면을 자주 먹는다.',
     '현대인의 삶에서 스마트폰은 필수품이 되었다.',
-    '현대인들 중에서 스마트폰을 사용하지 않는 사람은 거의 없다.',
-    '점심시간에 스마프폰을 이용해 영어 회화 공부를 하느라 혼자 밥을 먹는다.'
+    '현대인들 중에서 스마트폰을! 사용하지 않는 사람은 거의 없다.',
+    '점심시간에 스마프폰을 이용해 영어? 회화 공부를 하느라 혼자 밥을 먹는다.'
 ]
+
+# Stop word
+stopWordList = ('.,;!?')
+texts = [
+            [word for word in document.split()
+                if word not in stopWordList]
+            for document in documents
+]
+
+print(texts)
