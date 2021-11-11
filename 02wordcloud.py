29573798a02a04b4e9715458f67572782510da82
eurekasolution <nadopro@gmail.com>
2021-11-11 16:10:30 +0900

    03topic-2

--- 02wordcloud.py
+++ 02wordcloud.py
@@ -7,7 +7,7 @@ from gensim import corpora
 from gensim import models
 documents=[
     '나는 아침에 라면을 자주 먹는다.',
-    '나느 아침에 밥 대신에 라면을 자주 먹는다.',
+    '나는 아침에 밥 대신에 라면을 자주 먹는다.',
     '현대인의 삶에서 스마트폰은 필수품이 되었다.',
     '현대인들 중에서 스마트폰을! 사용하지 않는 사람은 거의 없다.',
     '점심시간에 스마프폰을 이용해 영어? 회화 공부를 하느라 혼자 밥을 먹는다.'
@@ -20,5 +20,15 @@ texts = [
                 if word not in stopWordList]
             for document in documents
 ]
+print("-"*80)
+print(documents)
 
+print("-"*80)
 print(texts)
+
+print("make Dictionary")
+dictionary = corpora.Dictionary(texts)
+print("make corpus") # 말뭉치, 단어사전
+corpus = [dictionary.doc2bow(text) for text in texts]
+print('corpus : {}'.format(corpus))
+
