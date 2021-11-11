8f2fb84a4a7d1cd4fe9dbf091b9084caaa9c4ecd
eurekasolution <nadopro@gmail.com>
2021-11-11 15:31:23 +0900

    git test

--- 01hello.py
+++ 01hello.py
@@ -7,4 +7,4 @@ from konlpy.tag import Kkma
 
 kkma = Kkma()
 print('kkma 문장 분리 :  ', kkma.sentences(u'안녕하세요. 홍길동입니다. 반갑습니다. 저는 인공지능입니다.'))
-#addeda
+print('kkma 문장 분리 :  ', kkma.sentences(u'깃허브 테스트를 수행하고 있습니다.'))
