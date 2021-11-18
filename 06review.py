7d48de0eac4792755af75f4498f762369d320657
eurekasolution <nadopro@gmail.com>
2021-11-18 10:16:21 +0900

    04word2vec 2nd 10:16

--- 06review.py
+++ 06review.py
@@ -5,3 +5,10 @@ from konlpy.tag import Okt
 from gensim.models import word2vec
 from konlpy.utils import pprint
 
+def read_data(filename): # d:/ai/review.txt
+    with codecs.open(filename, encodingn='utf-8', mode='r') as f:
+        data = [ line.split('\t') for line in f.read().splitlines()]
+        data = data[1:]
+    return data
+
+ratings_train = read_data("d:/ai/review.txt")
