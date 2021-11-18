a81af96a439c962c3d1122ccbbb9fa6ab833d05d
eurekasolution <nadopro@gmail.com>
2021-11-18 14:24:39 +0900

    04word2vec 2nd 14:24

--- 11loss.py
+++ 11loss.py
@@ -1,11 +1,14 @@
-#10tuning.py
+#11dataset.py
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import numpy as np
 
-x = np.array([1,2,3,4,5,6,7,8,9,10])
-y = np.array([1,2,3,4,5,6,7,8,9,10])
-z = np.array([11,12,13,14,15])
+x_train = np.array([1,2,3,4,5,6,7,8,9,10])
+y_train = np.array([1,2,3,4,5,6,7,8,9,10])
+x_test = np.array([11,12,13,14,15,16,17,18,19,20])
+y_test = np.array([11,12,13,14,15,16,17,18,19,20])
+
+z = np.array([111,112,113,114,115])
 
 
 model = Sequential()
@@ -16,12 +19,9 @@ model.add(Dense(4))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-model.fit(x, y, epochs=100,batch_size=1)
+model.fit(x_train, y_train, epochs=100,batch_size=1)
 
-mse = model.evaluate(x, y, batch_size=1)
-print('mse : ' , mse)
-
-loss, acc=model.evaluate(x,y, batch_size=1)
+loss, acc=model.evaluate(x_test,y_test, batch_size=1)
 print('loss : ', loss)
 print('acc : ', acc)
 
