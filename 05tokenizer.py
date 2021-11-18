76812741dd12bac39f5664da89dbd7bed7f50326
eurekasolution <nadopro@gmail.com>
2021-11-18 10:13:48 +0900

    04word2vec 2nd 10:13

--- 05tokenizer.py
+++ 05tokenizer.py
@@ -7,13 +7,13 @@ tagger = Okt()
 def tokenize(doc):
     return ['/'.join(t) for t in tagger.pos(doc, norm=True, stem=True)]
 
-sentences = tokenize(u'이것도 되나욕 ㅋㅋㅋ사릉해요')
+sentences = tokenize(u'이것도 되나욕 ㅋㅋㅋ 사릉해요 나는 당신을 사랑합니다. 나는 딥러닝을 공부하고 있습니다. 딥러닝 재미있다.')
 print('Result : ', sentences)
 
 print("명사, 형용사, 동사만 추출")
 nav_list = [word.split("/")[0] for word in sentences if
             word.split("/")[1] == "Noun" or
             word.split("/")[1] == "Adjective" or
-            word.split("/")[1] == "Verv"
+            word.split("/")[1] == "Verb"
             ]
 print(nav_list)
---
+++ 06review.py
@@ -0,0 +1,7 @@
+#06review.py
+
+import codecs
+from konlpy.tag import Okt
+from gensim.models import word2vec
+from konlpy.utils import pprint
+
