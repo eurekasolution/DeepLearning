bf4213613fd436be5fdc8f28a3abb21dbf006fec
eurekasolution <nadopro@gmail.com>
2021-11-18 15:54:59 +0900

    04word2vec 2nd 15:54

--- 13sentiment.py
+++ 13sentiment.py
@@ -9,3 +9,28 @@ def read_data(filename):
     with open(filename, 'r', encoding='utf-8') as f:
         result = [line.split('\t') for line in f.read().splitlines()]
         result = result[1:]
+    return result
+
+print("01 ... read file")
+train = read_data("d:/ai/ratings_train.txt")
+test = read_data("d:/ai/ratings_test.txt")
+
+print("02 ... word Embdding")
+def korean_movie(max_num_words=1000):
+    train_x = []
+    train_y = []
+
+    for i in range(len(train)):
+        train_x.append(train[i][1]) # 댓글 데이터
+        train_y.append(int(train[i][2])) # 긍정(1), 부정(0)
+
+    test_x = []
+    test_y = []
+
+    for i in range(len(test)):
+        test_x.append(test[i][1])  # 댓글 데이터
+        test_y.append(int(test[i][2]))  # 긍정(1), 부정(0)
+
+    # embedding
+    tokenizer = Tokenizer(num_words=max_num_words)
+    tokenizer.fit_on_texts(train_x)
