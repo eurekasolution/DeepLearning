10b1d62eb241f6d46c4d66af12e7a04482994ecd
eurekasolution <nadopro@gmail.com>
2021-11-18 12:37:25 +0900

    04word2vec 2nd 12:37

--- 09seq.py
+++ 09seq.py
@@ -11,8 +11,19 @@ model.add(Dense(5, input_dim=1, activation='relu'))
 model.add(Dense(3))
 model.add(Dense(1))
 
-model.compile(lose='mse', optimizer='adam')
+model.compile(loss='mse', optimizer='adam')  ## <-- loss 오타 났었습니다.
 model.fit(x, y, epochs=100,batch_size=1)
 
 mse = model.evaluate(x, y, batch_size=1)
 print('mse : ' , mse)
+
+# 할일
+# 1. 위 14라인 오타 수정
+# 2. 에러 해결하기
+# 에러 메시지를 보면
+# Microsoft C++ Redistributable for Visual Studio 2015, 2017 and 2019"
+# 재배포 패키지가 설치되어야 합니다.
+# 배포해 드렸던 파일에 보면
+# 02-VC_redist.x64 가 있습니다. 이 글 보시면 설치해 주시고,
+# 설치후 컴퓨터가 Reboot이 필요합니다.
+
