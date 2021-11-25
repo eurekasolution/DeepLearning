c6c79ea5266a7b142b99ddd4d6c5cc49ffa0f82f
eurekasolution <nadopro@gmail.com>
2021-11-25 12:20:47 +0900

    04word2vec 3rd 12:20

--- 16re.py
+++ 16re.py
@@ -14,7 +14,17 @@ import numpy as np
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+import pandas as pd
 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 # debug, warning, info, error, danger, critical (7)
+
+print("01... Data Loading")
+train = pd.read_table("d:/ai/simple_train.txt")
+test = pd.read_table("d:/ai/simple_test.txt")
+
+print("02... 데이터 확인")
+print("data type : ", type(train))
+print("# of data : ", len(train))
+print(train[0:10])
