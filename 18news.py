58f3d234d0bd73d7b5df6a07529d5e86dac2666d
eurekasolution <nadopro@gmail.com>
2021-11-25 16:13:54 +0900

    18news.py 3rd 16:13

--- 18news.py
+++ 18news.py
@@ -9,4 +9,8 @@ test_data = pd.read_csv("d:/ai/news_test.csv", encoding="utf-8")
 import matplotlib.pyplot as plt
 print("Step 1. 데이터 분포 분석")
 print("1-1. 분포도 확인")
-train_data['label'].value_counts().plot(kind='bar')
+#train_data['label'].value_counts().plot(kind='bar')
+
+print("1-2. 분포도 숫자로 확인")
+print("Train Data\n",train_data.groupby('label').size().reset_index(name='count'))
+print("Test Data\n",test_data.groupby('label').size().reset_index(name='count'))
